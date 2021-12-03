ee07a78d6e764b3091d439e6d1a741cc3d33caf5
MAC <mac@KIMELMAC.local>
2021-12-03 11:31:40 +0900

    Modify: Sider 수정

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ListPage from './pages/ListPage/ListPage';
 import Login from './pages/Login/Login';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/listPage" element={<ListPage />} />
+        {/* <Route path="/" element={<Login />} /> */}
       </Routes>
     </BrowserRouter>
   );
