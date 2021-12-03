ce7387c6fdda03fbf84b95f1304f3e3998cada9d
MAC <mac@KIMELMAC.local>
2021-12-03 22:29:41 +0900

    Add: 리스트 mock data 작업 / 기능 구현 중

--- src/pages/ListPage/ListPage.js
+++ src/pages/ListPage/ListPage.js
@@ -1,8 +1,11 @@
-import './ListPage.scss';
 import React, { useState, useEffect } from 'react';
+import LeftArticleBox from '../LeftArticleBox/LeftArticleBox';
+import TagKeyword from '../TagKeyword/TagKeyword';
+import './ListPage.scss';
 
 function ListPage() {
   const [listPageData, setListPageData] = useState([]);
+  const [recommendData, setRecommendData] = useState([]);
 
   useEffect(() => {
     fetch('/data/ListPageData.json')
@@ -10,65 +13,18 @@ function ListPage() {
       .then(res => setListPageData(res));
   }, []);
 
+  useEffect(() => {
+    fetch('/data/TagKeyword.json')
+      .then(res => res.json())
+      .then(res => setRecommendData(res));
+  }, []);
+
   return (
     <div className="ListPage">
-      <div className="wrapKeywordName">IT 트렌드</div>
-      <div className="keywordShell">
-        <div className="keywordList">
-          <div className="relatedKeyword">
-            <button className="detailKeyword">인공지능</button>
-            <span className="moreKeywords">
-              <em className="titleMore">더보기</em>
-              <span className="moreArrowBtn" />
-            </span>
-          </div>
-        </div>
-      </div>
+      <TagKeyword recommendKeyword={recommendData} />
       <section className="contentsContainer">
         <div className="wrapArticleBox">
-          {/* <div className="wrapArticleList">
-            <ul className="listArticle">
-              <li className="listImageAnimate">
-                <div className="postTitleAndImage">
-                  <h1 className="titleSubject">안녕하세요</h1>
-                  <div className="wrapSubContent">
-                    <p className="titleSub">"시간이 없어서"는 변명이다.</p>
-                    <span className="iconBar" />
-                    <span className="articleContent">
-                      긴지라 간에 용기가 같은 새 청춘 그림자는 웅대한 피가
-                      부패뿐이다. 보내는 생명을 일월과 긴지라 같은 속잎나고,
-                      밥을 인생에 교향악이다. 이것이야말로 피에 싸인 때문이다.
-                      보는 용감하고 얼마나 가장 같으며, 것이다. 낙원을 무한한
-                      인류의 따뜻한 꽃이 수 그들은 약동하다. 피어나기 그들은
-                      그들은 수 과실이 능히 커다란 든 거친 아름다우냐? 싸인 들어
-                      동력은 사막이다. 산야에 청춘에서만 대고, 사랑의 내려온
-                      있는 약동하다. 내는 용기가 주며, 방황하여도, 것은 그림자는
-                      끓는다.
-                    </span>
-                  </div>
-                  <span className="postAppend">
-                    <span className="nameText">공유</span>
-                    <span className="numText">0</span>
-                    <span className="iconDot" />
-                    <span className="nameText">댓글</span>
-                    <span className="numText">3</span>
-                    <span className="iconDot" />
-                    <span className="postTime">2시간전</span>
-                    <span className="iconDot" />
-                    <span className="iconBy">by</span>
-                    <span className="nameText">기멜</span>
-                  </span>
-                </div>
-                <div className="postThumb">
-                  <img
-                    src="images/ListPage/lighthouse.jpg"
-                    className="imageThumb"
-                    alt="포스트섬네일"
-                  />
-                </div>
-              </li>
-            </ul>
-          </div> */}
+          <LeftArticleBox articleBoxData={listPageData} />
           <div className="wrapWriterList">
             <p className="titleRecommend">추천작가</p>
             <div className="searchRecommend">
---
+++ src/pages/TagKeyword/TagKeyword.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import './TagKeyword.scss';
+
+function KeywordShell({ recommendKeyword }) {
+  const [moreActive, setMoreActive] = useState(false);
+
+  const ActiveBtn = () => {
+    setMoreActive(!moreActive ? true : false);
+  };
+
+  return (
+    <div
+      className="keywordShell"
+      style={!moreActive ? null : { height: '32px' }}
+    >
+      <div className="relatedKeyword">
+        <span className="wrapKeywordBtn">
+          {recommendKeyword.map(tagKeyword => {
+            const { id, keyword } = tagKeyword;
+            return (
+              <button className="detailKeyword" key={id}>
+                {keyword}
+              </button>
+            );
+          })}
+        </span>
+        <span className="moreKeywords">
+          <p className="titleMore" onClick={ActiveBtn}>
+            더보기
+          </p>
+          <span className="moreArrowBtn" />
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default KeywordShell;
