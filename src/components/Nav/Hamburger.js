447e9c447ced4fb48672090f453b2d4b6e9be3b6
Moro-yong <g76eho@gmail.com>
2021-12-07 01:35:19 +0900

    Modify: 로직 제거

--- src/components/Nav/Hamburger.js
+++ src/components/Nav/Hamburger.js
@@ -1,28 +1,26 @@
 import './Hamburger.scss';
 
-function Hamburger({ toggleValue }) {
-  console.log(toggleValue);
+function Hamburger() {
   return (
-    <section
-      className="Hamburger"
-      toggle={!toggleValue ? null : { display: 'none', left: '0' }}
-    >
-      <div className="top">
-        <img className="userImg" src="" alt="프로필사진" />
-        <span className="infoText">you can mak anyyhing by code</span>
-        <span className="infoName">- G.T.Brac -</span>
-        <span className="startButton">브랜치 시작하기</span>
-      </div>
-      <div className="buttom">
-        <ul className="list">
-          <li className="listIndex">브랜치 홈</li>
-          <li className="listIndex">브랜치 나우</li>
-        </ul>
-        <div className="bestCode">
-          오늘의 감성코드 보기
-          <div className="underLine" />
+    <section className="Hamburger">
+      <div className="HamburgerLeftContainer">
+        <div className="top">
+          <img className="userImg" src="" alt="프로필사진" />
+          <span className="infoText">you can mak anyyhing by code</span>
+          <span className="infoName">- G.T.Brac -</span>
+          <span className="startButton">브랜치 시작하기</span>
         </div>
-        <div className="findID">계정을 잊어버리셨나요?</div>
+        <div className="buttom">
+          <ul className="list">
+            <li className="listIndex">브랜치 홈</li>
+            <li className="listIndex">브랜치 나우</li>
+          </ul>
+          <div className="bestCode">
+            오늘의 감성코드 보기
+            <div className="underLine" />
+          </div>
+          <div className="findID">계정을 잊어버리셨나요?</div>
+        </div>
       </div>
     </section>
   );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,14 +6,15 @@ function Nav() {
   const [hamburgerToggle, setHamburgerToggle] = useState(false);
   // console.log(hamburgerToggle);
   const openHamburger = () => {
-    setHamburgerToggle(hamburgerToggle === false ? true : false);
+    setHamburgerToggle(!hamburgerToggle);
   };
-  // console.log(hamburgerToggle);
+
   return (
     <>
       <section className="nav">
         <div className="navLeft">
           <img
+            onClick={openHamburger}
             className="hamburgerButton"
             src="/images/Nav/iconmonstr-menu-thin.svg"
             alt="더보기"
@@ -24,13 +25,12 @@ function Nav() {
           <span className="startButton">시작하기</span>
           <img
             className="searchIcon"
-            onClick={openHamburger}
             src="/images/Nav/iconmonstr-search-thin.svg"
             alt="검색"
           />
         </div>
       </section>
-      <Hamburger toggleValue={hamburgerToggle} />
+      {hamburgerToggle && <Hamburger />}
     </>
   );
 }
