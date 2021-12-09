b7ae11368f10664a21bc9efec869eb42aa0e7971
Moro-yong <g76eho@gmail.com>
2021-12-09 17:34:13 +0900

    Modify: config 링크 수정

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const BASE_URL = 'http://10.58.3.206:8000';
 export const API = {
   MAIN: `${BASE_URL}/main`,
   MYPAGE: `${BASE_URL}/users/private/user`,
-  USER_PAGE: `${BASE_URL}/users/public/user/`,
+  USER_PAGE: `${BASE_URL}/users/public/user`,
   SIGNUP: `${BASE_URL}/users/signup`,
   WRITERDATA: `${BASE_URL}/users/?user_tag_id=`,
   TAGDATA: `${BASE_URL}/branch_tags/userTagList`,
